fba32804c4e29e050764ff96ce61cbcd56daf69e
BullDuckling <tbuhla@gmail.com>
2018-11-27 13:37:45 +0100

    cnn_rnn baseline

---
+++ models/cnn_rnn/imdb_cnn_rnn.py
@@ -0,0 +1,80 @@
+from keras.preprocessing import sequence
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import SimpleRNN
+from keras.layers import Conv1D, MaxPooling1D
+from datasets import imdb
+
+# Embedding
+from utils.data_utils import export
+
+max_features = 20000
+maxlen = 100
+embedding_size = 128
+
+# Convolution
+kernel_size = 5
+filters = 64
+pool_size = 4
+
+# RNN
+rnn_output_size = 70
+
+# Training
+batch_size = 30
+epochs = 1
+
+'''
+Note:
+batch_size is highly sensitive.
+Only 2 epochs are needed as the dataset is very small.
+'''
+
+print('Loading data...')
+(x_train, y_train), (x_val, y_val), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_val), 'validation sequences')
+print(len(x_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_val = sequence.pad_sequences(x_val, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+print('x_train shape:', x_train.shape)
+print('x_val shape:', x_val.shape)
+print('x_test shape:', x_test.shape)
+
+print('Build model...')
+
+model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(SimpleRNN(rnn_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print('Train...')
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    callbacks=[EarlyStopping(patience=2)],
+                    validation_data=(x_val, y_val))
+score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+export(model, history)
